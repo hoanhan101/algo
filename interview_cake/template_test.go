64c7794ea1e0ff3cf1beb13445f723778e970ec9
Hoanh An <hoanhan@bennington.edu>
2019-07-22 15:15:15 +0700

    add in a template file

---
+++ interview_cake/template_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTemplate(t *testing.T) {
+	tests := []struct {
+		in       string
+		expected bool
+	}{
+		{"is this a template", true},
+		{"is this not a template", false},
+	}
+
+	for _, tt := range tests {
+		result := isTemplate(tt.in)
+		if !reflect.DeepEqual(result, tt.expected) {
+			t.Errorf("should be %v instead %v", tt.expected, result)
+		}
+	}
+}
+
+func isTemplate(question string) bool {
+	if question == "is this a template" {
+		return true
+	}
+	return false
+}
