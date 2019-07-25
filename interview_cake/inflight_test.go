c232e915db8290a44e3991143afd227a614fc4d7
Hoanh An <hoanhan@bennington.edu>
2019-07-25 10:57:00 +0700

    inflight movies

---
+++ interview_cake/inflight_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFillFlight(t *testing.T) {
+	tests := []struct {
+		in1      []int
+		in2      int
+		expected bool
+	}{
+		{[]int{}, 1, false},
+		{[]int{0}, 1, false},
+		{[]int{0, 1}, 1, true},
+		{[]int{2, 3, 4}, 6, true},
+		{[]int{2, 3, 4}, 8, false},
+	}
+
+	for _, tt := range tests {
+		result := fillFlight(tt.in1, tt.in2)
+		if !reflect.DeepEqual(result, tt.expected) {
+			t.Errorf("should be %v instead %v", tt.expected, result)
+		}
+	}
+}
+
+func fillFlight(movieLengths []int, flightLength int) bool {
+	movies := map[int]int{}
+
+	for _, v := range movieLengths {
+		// return true if we've seen the movie, else add the movie in the
+		// watched list
+		matchLength := flightLength - v
+		if _, ok := movies[matchLength]; ok {
+			return true
+		} else {
+			movies[v] = 1
+		}
+	}
+
+	return false
+}
