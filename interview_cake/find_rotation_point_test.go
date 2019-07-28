6054dfb47ca7b29553bec57073957feaf0f36e42
Hoanh An <hoanhan@bennington.edu>
2019-07-28 12:40:26 +0700

    find rotation point

---
+++ interview_cake/find_rotation_point_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindRotationPoint(t *testing.T) {
+	tests := []struct {
+		in       []string
+		expected int
+	}{
+		{
+			[]string{
+				"ptolemaic",
+				"retrograde",
+				"supplant",
+				"undulate",
+				"xenoepist",
+				"asymptote", // <-- rotates here!
+				"babka",
+				"banoffee",
+				"engender",
+				"karpatka",
+				"othellolagkage",
+			},
+			5,
+		},
+		{
+			[]string{
+				"xenoepist",
+				"asymptote", // <-- rotates here!
+				"babka",
+				"banoffee",
+				"engender",
+				"karpatka",
+				"othellolagkage",
+			},
+			1,
+		},
+	}
+
+	for _, tt := range tests {
+		result := findRotationPoint(tt.in)
+		if !reflect.DeepEqual(result, tt.expected) {
+			t.Errorf("should be %v instead %v", tt.expected, result)
+		}
+	}
+}
+
+// findRotationPoint returns the index of the rotation point from a list of
+// words.
+func findRotationPoint(words []string) int {
+	firstWord := words[0]
+	floorIndex := 0
+	ceilingIndex := len(words) - 1
+
+	for floorIndex < ceilingIndex {
+		guessIndex := floorIndex + (ceilingIndex-floorIndex)/2
+
+		// if the guess word comes after the first word or the first word, then
+		// go right. otherwise, go left.
+		if words[guessIndex] >= firstWord {
+			floorIndex = guessIndex
+		} else {
+			ceilingIndex = guessIndex
+		}
+
+		// floorIndex and ceilingIndex have converged at rotation point.
+		if floorIndex+1 == ceilingIndex {
+			return ceilingIndex
+		}
+	}
+
+	return ceilingIndex
+}
