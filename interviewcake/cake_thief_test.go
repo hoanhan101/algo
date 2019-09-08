7ed8fb8ac5313c9cc39afd0626e3d1fb6750c7e0
Hoanh An <hoanhan@bennington.edu>
2019-09-08 17:37:48 -0400

    add cake thief problem

---
+++ interviewcake/cake_thief_test.go
@@ -0,0 +1,22 @@
+// Problem:
+// Given a list of cakes where each has its own weight and price, and a weight
+// capacity, return the maximum price that one can purchase. Note that, weight
+// and price can be non-negative integer.
+//
+// Example:
+// Input: []cake{{7, 160}, (3, 90), (2, 15)}, capacity=20
+// Output: 555, because for a bag that weights 20 lbs, 6 cakes that weights 3
+// lbs and $90 each and 1 cake that weights 2 lbs and $15 cost the most.
+//
+// Solution:
+// TODO
+//
+// Cost:
+// TODO
+
+package interviewcake
+
+// import (
+// 	"reflect"
+// 	"testing"
+// )
