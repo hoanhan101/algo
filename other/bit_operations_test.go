b2c43052aaff732dc973743844daf27839242ee6
Hoanh An <hoanhan@bennington.edu>
2019-10-02 22:44:48 -0400

    add set bit operation

--- other/bit_operations_test.go
+++ other/bit_operations_test.go
@@ -16,14 +16,13 @@ func TestGetBit(t *testing.T) {
 		in2      int
 		expected uint
 	}{
+		{6, 0, 0},
 		{6, 1, 1},
 		{6, 2, 1},
 		{6, 3, 0},
 		{6, 4, 0},
 		{6, 5, 0},
 		{6, 6, 0},
-		{6, 7, 0},
-		{6, 8, 0},
 	}
 
 	for _, tt := range tests {
@@ -32,16 +31,47 @@ func TestGetBit(t *testing.T) {
 	}
 }
 
+func TestSetBit(t *testing.T) {
+	tests := []struct {
+		in1      int
+		in2      int
+		expected int
+	}{
+		{6, 0, 7},
+		{6, 1, 6},
+		{6, 2, 6},
+		{6, 3, 14},
+		{6, 4, 22},
+		{6, 5, 38},
+		{6, 6, 70},
+	}
+
+	for _, tt := range tests {
+		result := setBit(tt.in1, tt.in2)
+		common.Equal(t, tt.expected, result)
+	}
+}
+
 // getBit returns the value of bit ith for a given number.
 func getBit(number, i int) uint {
-	// perform an AND with number to clear all bits other than the one at bit
-	// i.
+	// shift 1 over by i bits, creating a bitmask value.
 	mask := 1 << uint(i)
 
+	// perform an AND with number to clear all bits other than the one at bit i.
 	// if the value is not 0, bit 1 must have a 1.
 	if number&mask != 0 {
 		return 1
 	}
 
 	return 0
+}
+
+// setBit sets the value of bit ith for a given number.
+func setBit(number, i int) int {
+	// shift 1 over by i bits, creating a bitmask value.
+	mask := 1 << uint(i)
+
+	// perform an OR with number to change the number at bit i without
+	// affecting other values.,
+	return number | mask
 }
