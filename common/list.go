0831d4adaaa0406e252899d226e62a1e19bda350
Hoanh An <hoanhan@bennington.edu>
2019-11-15 01:08:21 -0500

    move linked list helper func to common

--- common/list.go
+++ common/list.go
@@ -1,7 +1,18 @@
 package common
 
-// ListNode represents a node in a Linked list.
+// ListNode represents a node in a linked list.
 type ListNode struct {
 	Value int
 	Next  *ListNode
+}
+
+// LinkedListToSlice converts a linked list into an array of integer.
+func LinkedListToSlice(node *ListNode) []int {
+	out := []int{}
+
+	for n := node; n != nil; n = n.Next {
+		out = append(out, n.Value)
+	}
+
+	return out
 }
