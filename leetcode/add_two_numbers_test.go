a5fe621961f8a706aad66253bced66c899528282
Hoanh An <hoanhan@bennington.edu>
2019-11-16 01:43:29 -0500

    add two numbers

---
+++ leetcode/add_two_numbers_test.go
@@ -0,0 +1,121 @@
+/*
+Problem:
+- Given two linked lists representing two non-negative number, add them together
+  and return it as a linked list.
+
+Assumption:
+- The digits are stored in reverse order.
+- Each node contains a single digit.
+
+Example:
+- Input: (1 -> 6 -> 4) + (2 -> 4-> 1)
+  Output: (3 -> 0 -> 6)
+
+Approach:
+- Traverse both lists and keep track of the sum and carry for each
+  digit.
+
+Solution:
+- Initialize dummy heads to keep track of the head nodes.
+- While either list is not empty, calculate the sum of corresponding
+  digits, update it carry at each step and add it after the current node.
+- If the sum have an extra carry at the end, simply make a new node and
+  add it in the end.
+
+Cost:
+- O(n|m) time, O(m|n) space where m and m are lengths of these two lists.
+*/
+
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/hoanhan101/algo/common"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	t11 := &common.ListNode{1, nil}
+	t11.Next = &common.ListNode{6, nil}
+	t11.Next.Next = &common.ListNode{4, nil}
+
+	t12 := &common.ListNode{2, nil}
+	t12.Next = &common.ListNode{4, nil}
+	t12.Next.Next = &common.ListNode{1, nil}
+
+	t13 := &common.ListNode{3, nil}
+	t13.Next = &common.ListNode{0, nil}
+	t13.Next.Next = &common.ListNode{6, nil}
+
+	t21 := &common.ListNode{9, nil}
+	t21.Next = &common.ListNode{9, nil}
+
+	t22 := &common.ListNode{1, nil}
+
+	t23 := &common.ListNode{0, nil}
+	t23.Next = &common.ListNode{0, nil}
+	t23.Next.Next = &common.ListNode{1, nil}
+
+	tests := []struct {
+		in1      *common.ListNode
+		in2      *common.ListNode
+		expected *common.ListNode
+	}{
+		{t11, t12, t13},
+		{t21, t22, t23},
+	}
+
+	for _, tt := range tests {
+		common.Equal(
+			t,
+			tt.expected,
+			addTwoNumbers(tt.in1, tt.in2),
+		)
+	}
+}
+
+func addTwoNumbers(l1, l2 *common.ListNode) *common.ListNode {
+	// initialize dummy heads to keep track of the head nodes.
+	dummyHead := &common.ListNode{}
+	current := dummyHead
+	p1 := l1
+	p2 := l2
+	carry := 0
+
+	// while either list is not empty.
+	for p1 != nil || p2 != nil {
+		// if p1 or p2 is nil, use 0 as its value.
+		p1v, p2v := 0, 0
+		if p1 != nil {
+			p1v = p1.Value
+		}
+		if p2 != nil {
+			p2v = p2.Value
+		}
+
+		// calculate the sum of corresponding digits and update it carry.
+		sum := carry + p1v + p2v
+		digit := sum % 10
+		carry = sum / 10
+
+		// add a next node the current node.
+		current.Next = &common.ListNode{digit, nil}
+		current = current.Next
+
+		// keep traversing as long as we have not reached the end.
+		if p1 != nil {
+			p1 = p1.Next
+		}
+		if p2 != nil {
+			p2 = p2.Next
+		}
+	}
+
+	// if the sum have an extra carry at the end, simply make a new node and
+	// add it in the end.
+	if carry > 0 {
+		current.Next = &common.ListNode{carry, nil}
+	}
+
+	return dummyHead.Next
+}
