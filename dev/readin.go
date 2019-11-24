286d8f4024ed75c9a4c6f8fb283813fe6ea4a6d9
Hoanh An <hoanhan@bennington.edu>
2019-11-24 15:14:37 -0500

    add a dev dir for quick testing and prototyping things

---
+++ dev/readin.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// initialize a scanner to read form stdin.
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// scan then print out the output.
+	if scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
