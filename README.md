9b639dcc0aa10acfdc97456d1aa90d7661a5a792
Hoanh An <hoanhan@bennington.edu>
2019-12-18 12:56:28 -0500

    happy number

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 > [Buy me a coffee!](https://www.buymeacoffee.com/aHjIWu6Ck)
 
-## Total questions with solutions: 83
+## Total questions with solutions: 84
 
 - Every week, I send out 4-6 programming problems with detailed solutions, test
   cases, and program analysis, all in Go. 
@@ -165,6 +165,7 @@
   - Fast and slow pointers
     - [Linked list cycle](gtci/linked_list_cycle_test.go)
     - [Start of a linked list cycle](gtci/cycle_start_test.go)
+    - [Happy number](gtci/happy_number_test.go)
 - **Other**
   - Data structures
     - Linked List
---
+++ gtci/happy_number_test.go
@@ -0,0 +1,80 @@
+/*
+Problem:
+- Write an algorithm to determine if a number is happy.
+- Any number will be called a happy number if, after repeatedly replacing
+  it with a number equal to the sum of the square of all of its digits,
+  leads us to 1.
+
+Example:
+- Input: 19
+  Output: true
+  Explanation:
+	  1^2 + 9^2 = 82
+	  8^2 + 2^2 = 68
+	  6^2 + 8^2 = 100
+	  1^2 + 0^2 + 0^2 = 1
+
+Approach:
+- Since the process always end in a cycle, we can use a similar approach to
+  finding a cycle in linked list problem.
+- Once is cycle is found, check if it is stuck on 1.
+
+Cost:
+- O(n) time, O(1) space.
+*/
+
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/hoanhan101/algo/common"
+)
+
+func TestIsHappy(t *testing.T) {
+	tests := []struct {
+		in       int
+		expected bool
+	}{
+		{23, true},
+		{19, true},
+		{12, false},
+	}
+
+	for _, tt := range tests {
+		common.Equal(
+			t,
+			tt.expected,
+			isHappy(tt.in),
+		)
+	}
+}
+
+func isHappy(num int) bool {
+	slow, fast := num, num
+
+	for true {
+		slow = squareSum(slow)
+		fast = squareSum(squareSum(fast))
+
+		if slow == fast {
+			break
+		}
+	}
+
+	return slow == 1
+}
+
+func squareSum(num int) int {
+	sum := 0
+
+	for num > 0 {
+		lastDigit := num % 10
+		sum += lastDigit * lastDigit
+
+		// get rid of last digit.
+		num /= 10
+	}
+
+	return sum
+}
