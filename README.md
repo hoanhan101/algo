7ee1e678b8a30f1dd29bd5070a087ef46b76aaa6
Hoanh An <hoanhan@bennington.edu>
2019-12-18 18:39:37 -0500

    palindrome linked list

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 > [Buy me a coffee!](https://www.buymeacoffee.com/aHjIWu6Ck)
 
-## Total questions with solutions: 85
+## Total questions with solutions: 86
 
 - Every week, I send out 4-6 programming problems with detailed solutions, test
   cases, and program analysis, all in Go. 
@@ -167,6 +167,7 @@
     - [Start of a linked list cycle](gtci/cycle_start_test.go)
     - [Happy number](gtci/happy_number_test.go)
     - [Middle of a linked list](gtci/middle_list_test.go)
+    - [Palindrome linked list](gtci/palindrome_list_test.go)
 - **Other**
   - Data structures
     - Linked List
---
+++ gtci/palindrome_list_test.go
@@ -0,0 +1,105 @@
+/*
+Problem:
+- Given the head of a singly linked list, write a function to determine
+  if it is a palindrome in constant space.
+
+Approach:
+- Find the middle of the linked list and reverse a half list
+- After comparing the first half with the reversed half to check if it's
+  a palindrome, revert to the half to original form.
+
+Cost:
+- O(n) time, O(1) space.
+*/
+
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/hoanhan101/algo/common"
+)
+
+func TestIsPalindromeList(t *testing.T) {
+	t1 := common.NewListNode(1)
+	t1.AddNext(1)
+	t1.AddNext(1)
+	t1.AddNext(1)
+	t1.AddNext(1)
+
+	t2 := common.NewListNode(1)
+	t2.AddNext(2)
+	t2.AddNext(3)
+	t2.AddNext(2)
+	t2.AddNext(1)
+
+	t3 := common.NewListNode(1)
+	t3.AddNext(2)
+	t3.AddNext(3)
+	t3.AddNext(4)
+	t3.AddNext(5)
+
+	tests := []struct {
+		in       *common.ListNode
+		expected bool
+	}{
+		{t1, true},
+		{t2, true},
+		{t3, false},
+	}
+
+	for _, tt := range tests {
+		common.Equal(
+			t,
+			tt.expected,
+			isPalindrome(tt.in),
+		)
+	}
+}
+
+func isPalindrome(head *common.ListNode) bool {
+	slow, fast := head, head
+
+	// find the middle of the linked list.
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+	}
+
+	// reverse a half.
+	headHalf := reverse(slow)
+	copyHalf := headHalf
+
+	// compare the first and second half.
+	for head != nil && headHalf != nil {
+		if head.Value != headHalf.Value {
+			return false
+		}
+
+		head = head.Next
+		headHalf = headHalf.Next
+	}
+
+	// revert the half to its original form.
+	_ = reverse(copyHalf)
+
+	if head == nil && headHalf == nil {
+		return true
+	}
+
+	return false
+}
+
+func reverse(head *common.ListNode) *common.ListNode {
+	var prev *common.ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	return prev
+}
