bf8646fe2d6d751c609670f4936c83c5bd69fb92
Hoanh An <hoanhan@bennington.edu>
2020-01-05 18:59:02 -0500

    fix typo

--- gtci/square_sorted_array_test.go
+++ gtci/square_sorted_array_test.go
@@ -1,5 +1,6 @@
 /*
-Problem: - Given a sorted array, create a new array containing squares of all the
+Problem:
+- Given a sorted array, create a new array containing squares of all the
   number of the input array in the sorted order.
 - Note that the input can have negative numbers.
 
