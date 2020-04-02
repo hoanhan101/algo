5136beaaf754c0d6a488c446ad0711b26fab087d
Hoanh An <hoanhan101@gmail.com>
2020-04-01 22:58:07 -0400

    equal subset sum partition brute-foce

---
+++ gtci/equal_subset_partition_test.go
@@ -0,0 +1,164 @@
+/*
+Problem: TODO
+- Given a set of items, each with a weight and a value, determine the number of
+  each item to include in a collection so that the total weight is less or equal
+  than a given limit and the total value is as large as possible.
+
+Example: TODO
+- Input: a set of fruit items, with their weights and profits as follow
+    fruit  : apple | orange | banana | melon
+	weight :   2   |    3   |    1   |   4
+	profit :   4   |    5   |    3   |   7
+    & knapsack capacity = 5
+  Output: banana & melon
+  Explanation: banana and melon gives the maximum profit of 10 and weight exactly 5
+- Input: weight :   1   |    6   |    10   |   16
+	profit :   1   |    2   |    3    |   5
+    & knapsack capacity = 7
+  Output: 22
+  Explanation: 16+6 gives the largest profit and weights exactly 7
+
+Brute-force approach: TODO
+- First, calculate the profit for the item at the current index.
+- If the total weight does not exceed the capacity, recursively process
+  the remaining capacity and items.
+- Second, recursively process after excluding the item at the current index.
+- Return the higher profit between these two.
+
+Cost: TODO
+- Brute-force: O(2^n) time, O(n) space.
+- Top-down: O(n*c) time, O(n*c) space where n is the number of items, c is the knapsack capacity.
+- Bottom-up: O(n*c) time, O(n*c) space where n is the number of items, c is the knapsack capacity.
+*/
+
+package gtci
+
+import (
+	"testing"
+
+	"github.com/hoanhan101/algo/common"
+)
+
+func TestCanPartition(t *testing.T) {
+	tests := []struct {
+		in       []int
+		expected bool
+	}{
+		{[]int{1, 2, 3, 4}, true},
+		{[]int{1, 1, 3, 4, 7}, true},
+		{[]int{2, 3, 4, 6}, false},
+	}
+
+	for _, tt := range tests {
+		common.Equal(
+			t,
+			tt.expected,
+			canPartitionBF(tt.in),
+		)
+	}
+}
+
+func canPartitionBF(nums []int) bool {
+	s := 0
+	for _, v := range nums {
+		s += v
+	}
+
+	// return false if the total sum is odd.
+	if s%2 != 0 {
+		return false
+	}
+
+	return canPartitionBFRecur(nums, s/2, 0)
+}
+
+func canPartitionBFRecur(nums []int, sum, currentIndex int) bool {
+	if sum == 0 {
+		return true
+	}
+
+	n := len(nums)
+	if n == 0 || currentIndex >= n {
+		return false
+	}
+
+	if nums[currentIndex] <= sum {
+		if canPartitionBFRecur(nums, sum-nums[currentIndex], currentIndex+1) {
+			return true
+		}
+	}
+
+	return canPartitionBFRecur(nums, sum, currentIndex+1)
+}
+
+// func knapsackTD(profits, weights []int, capacity int) int {
+// 	// since for each recursive call, only capacity and current index change,
+// 	// can have a 2D array for memoization.
+// 	memo := make([][]int, len(profits))
+// 	for i := range memo {
+// 		memo[i] = make([]int, capacity+1)
+// 	}
+
+// 	return knapsackTDMemo(memo, profits, weights, capacity, 0)
+// }
+
+// func knapsackTDMemo(memo [][]int, profits, weights []int, capacity, currentIndex int) int {
+// 	if capacity <= 0 || currentIndex >= len(profits) {
+// 		return 0
+// 	}
+
+// 	// return immediately if found in cache.
+// 	if memo[currentIndex][capacity] != 0 {
+// 		return memo[currentIndex][capacity]
+// 	}
+
+// 	// calculate the profit for the item at the current index.
+// 	profit1 := 0
+// 	if weights[currentIndex] <= capacity {
+// 		profit1 = profits[currentIndex] + knapsackTDMemo(memo, profits, weights, capacity-weights[currentIndex], currentIndex+1)
+// 	}
+
+// 	// process after excluding the item at the current index.
+// 	profit2 := knapsackTDMemo(memo, profits, weights, capacity, currentIndex+1)
+
+// 	return common.Max(profit1, profit2)
+// }
+
+// func knapsackBU(profits, weights []int, capacity int) int {
+// 	n := len(profits)
+// 	if capacity <= 0 || n == 0 || len(weights) != n {
+// 		return 0
+// 	}
+
+// 	memo := make([][]int, n)
+// 	for i := range memo {
+// 		memo[i] = make([]int, capacity+1)
+// 	}
+
+// 	// if we have only one weight, we will take it if it is not more than the
+// 	// capacity.
+// 	for i := 0; i < capacity+1; i++ {
+// 		if weights[0] <= i {
+// 			memo[0][i] = profits[0]
+// 		}
+// 	}
+
+// 	for i := 1; i < n; i++ {
+// 		for c := 1; c < capacity+1; c++ {
+// 			profit1, profit2 := 0, 0
+
+// 			// include the item if it's not bigger than the capacity.
+// 			if weights[i] <= c {
+// 				profit1 = profits[i] + memo[i-1][c-weights[i]]
+// 			}
+
+// 			// exclude the item.
+// 			profit2 = memo[i-1][c]
+
+// 			// take the maximum.
+// 			memo[i][c] = common.Max(profit1, profit2)
+// 		}
+// 	}
+
+// 	return memo[n-1][capacity]
+// }
